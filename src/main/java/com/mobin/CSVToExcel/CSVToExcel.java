ba6993f81d33134a92d32442f9ac38cd8c652660
mobin <996833461@qq.com>
2016-08-07 14:27:30 +0800

    优化代码

--- src/main/java/com/mobin/CSVToExcel/CSVToExcel.java
+++ src/main/java/com/mobin/CSVToExcel/CSVToExcel.java
@@ -20,7 +20,11 @@ public class CSVToExcel {
     private Map<String, Integer> mapRow = new HashMap<>();   //值所在行
     private Map<String, Integer> mapCol = new HashMap<>();   //表头所在列
     private String[] str;
-    private String[] value_1;
+    private Integer lineRow;
+    private Integer lineCol;
+    private int cellnum;
+    private Row row;
+    private Row curr_row;
 
     public void convert(String originPath, String targetPath) {
         try (InputStream input = new FileInputStream(originPath);//源转换文件如:f:\\test
@@ -34,22 +38,21 @@ public class CSVToExcel {
             //插入数据
             for (String data = bufferedReader.readLine(); data != null; data = bufferedReader.readLine()) {
                 str = data.split(",");
-                value_1 = str[1].split("~");
-                int cellnum_1 = Integer.valueOf(value_1[1]);//截取拼接在后面的数字，该数字表示值和表头放在哪列
-
-                Integer lineRow = mapRow.get(str[0]);   //获取该值所对应的行，如果为空表示是新行
-                Integer lineCol = mapCol.get(value_1[0]);   //获取表头所对应的列，如果为空表示是新表头字段
-                if ((lineRow != null)) {//同一行
-                    Row curr_row = sheet.getRow(lineRow);
-                    isaddHeader(sheet, lineCol, cellnum_1, value_1[0]);
-                    curr_row.createCell(cellnum_1).setCellValue(str[2]);
+                 lineRow = mapRow.get(str[0]);   //获取该值所对应的行，如果为空表示是新行
+                 lineCol = mapCol.get(str[1]);   //获取表头所对应的列，如果为空表示是新表头字段
+                if ((lineRow != null)) {                                 //同一行
+                    curr_row = sheet.getRow(lineRow);
+                    isaddHeader(sheet, lineCol,str[1]);
+                    cellnum = mapCol.get(str[1]);
+                    curr_row.createCell(cellnum).setCellValue(str[2]);
                 } else {                                                           //不同行
                     ++lineNum;
                     mapRow.put(str[0], lineNum);
-                    Row row = sheet.createRow(lineNum);
+                    row = sheet.createRow(lineNum);
                     row.createCell(0).setCellValue(str[0]);  //第一列值
-                    isaddHeader(sheet, lineCol, cellnum_1, value_1[0]);
-                    row.createCell(cellnum_1).setCellValue(str[2]);
+                    isaddHeader(sheet, lineCol,str[1]);  //添加表头
+                    cellnum = mapCol.get(str[1]);
+                    row.createCell(cellnum).setCellValue(str[2]);
                 }
             }
             wb.write(out);
@@ -61,10 +64,10 @@ public class CSVToExcel {
         System.out.println("转换完成！");
     }
 
-    public void isaddHeader(Sheet sheet, Integer lineCol, int cellnum_1, String value_1) {
+    public void isaddHeader(Sheet sheet, Integer lineCol, String value_1) {
         if (lineCol == null) {                                                                         //说明该表头字段不存在
             mapCol.put(value_1, ++colNum);
-            sheet.getRow(0).createCell(cellnum_1).setCellValue(value_1);  //添加新表头字段
+            sheet.getRow(0).createCell(colNum).setCellValue(value_1);  //添加新表头字段
         }
     }
 
